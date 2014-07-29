234c17a942c99a979b6d187c558158b8a1c7b8c2
vadimbulavski <zolsberh@mail.ru>
2014-07-29 15:39:46 +0200

    homework 8

--- DZ8/8_2/main.c
+++ DZ8/8_2/main.c
@@ -3,23 +3,26 @@
 
 int main()
 {
-    int S=1000, m=3, Pp=6, Sp=4;
+    int S=1000, m=3, Pp=6, Sp=4, P;
     double V=1000;
-    S+=S*Pp/100;
+    P=S*Pp/100;
+    S+=P;
     while (V<=S)
     {
         printf("Mesyac: %d\n", m);
         printf("Sasha = %d\n", S);
         V+=V*Sp/100;
+        S+=P;
         printf("Vasya = %.2f\n", V);
         if (V<S)
         {
-            for (int i=0; i<=3; ++i)
+            for (int i=0; i<=2; ++i)
             {
                 m+=3;
                 printf("Mesyac: %d\n", m);
                 printf("Sasha = %d\n", S);
                 V+=V*Sp/100;
+                S+=P;
                 printf("Vasya = %.2f\n", V);
             }
         }
