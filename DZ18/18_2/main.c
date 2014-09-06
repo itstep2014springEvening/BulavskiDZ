835a145dd554761019c32b1715dc0058c2360e85
vadimbulavski <zolsberh@mail.ru>
2014-09-06 11:10:39 +0300

    homework 18

---
+++ DZ18/18_2/main.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "myteam.h"
+
+int main()
+{
+    const int N=100;
+    int x=0,i=0;
+    int memory[N],InstrRegistor=0;
+    int akkamulyator=0;
+    int AdrRegistor;
+    int OperRegistor;
+    int top=100, bias=0;
+    printf("Hello, user!\nenter your team:\n");
+    while (x!=-99999)
+    {
+        printf("%02d ", i);
+        scanf("%d", &x);
+        memory[i]=x;
+        ++i;
+    }
+    printf("Go!\n");
+    do
+    {
+        if (memory[InstrRegistor]<0)
+        {
+            OperRegistor=(-memory[InstrRegistor]/100) ;
+        }
+        else
+        {
+            OperRegistor=(memory[InstrRegistor]/100);
+        }
+        AdrRegistor=memory[InstrRegistor]%100;
+        switch (OperRegistor)
+        {
+        case READ:
+            scanf("%d",&memory[AdrRegistor+bias]);
+            InstrRegistor++;
+            break;
+        case WRITE:
+            printf("%+05d\n",memory[AdrRegistor+bias]);
+            InstrRegistor++;
+            break;
+        case LOAD:
+            akkamulyator=memory[AdrRegistor+bias];
+            InstrRegistor++;
+            break;
+        case STORE:
+            memory[AdrRegistor+bias]=akkamulyator;
+            InstrRegistor++;
+            break;
+        case LOADTOP:
+            akkamulyator=top;
+            InstrRegistor++;
+            break;
+        case STORETOP:
+            top=akkamulyator;
+            InstrRegistor++;
+            break;
+        case LOADBIAS:
+            akkamulyator=bias;
+            InstrRegistor++;
+            break;
+        case STOREBIAS:
+            bias=akkamulyator;
+            InstrRegistor++;
+            break;
+        case PUSH:
+            memory[--top]=akkamulyator;
+            InstrRegistor++;
+            break;
+        case POP:
+            akkamulyator=memory[top++];
+            InstrRegistor++;
+            break;
+        case ADD:
+            akkamulyator+=memory[AdrRegistor+bias];
+            InstrRegistor++;
+            break;
+        case SUBSTRACT:
+            akkamulyator-=memory[AdrRegistor+bias];
+            InstrRegistor++;
+            break;
+        case DIVIDE:
+            akkamulyator/=memory[AdrRegistor+bias];
+            InstrRegistor++;
+            break;
+        case MULTIPLY:
+            akkamulyator*=memory[AdrRegistor+bias];
+            InstrRegistor++;
+            break;
+        case LITERAL:
+            akkamulyator=AdrRegistor;
+            InstrRegistor++;
+            break;
+        case BRANCH :
+            InstrRegistor=AdrRegistor;
+            break;
+        case BRANCHNEG:
+            if (akkamulyator<0)
+                InstrRegistor=AdrRegistor;
+            else
+                InstrRegistor++;
+            break;
+        case BRANCHZERO:
+            if (akkamulyator==0)
+                InstrRegistor=AdrRegistor;
+            else
+                InstrRegistor++;
+            break;
+        case HALT:
+            printf("end\n");
+            break;
+        case CALL:
+            memory[--top]=InstrRegistor;
+            InstrRegistor=AdrRegistor;
+            break;
+        case RETURN:
+            InstrRegistor=memory[top++]+1;
+            top+=AdrRegistor;
+            break;
+        }
+    }
+    while(OperRegistor!=43);
+    return 0;
+}
---
+++ DZ18/18_2/myteam.h
@@ -0,0 +1,26 @@
+#ifndef MYTEAM_H_INCLUDED
+#define MYTEAM_H_INCLUDED
+
+#define READ 10
+#define WRITE 11
+#define LOAD 20
+#define STORE 21
+#define LOADTOP 22
+#define STORETOP 23
+#define LOADBIAS 24
+#define STOREBIAS 25
+#define PUSH 26
+#define POP 27
+#define ADD 30
+#define SUBSTRACT 31
+#define DIVIDE 32
+#define MULTIPLY 33
+#define LITERAL 34
+#define BRANCH 40
+#define BRANCHNEG 41
+#define BRANCHZERO 42
+#define HALT 43
+#define CALL 44
+#define RETURN 45
+
+#endif // MYTEAM_H_INCLUDED
